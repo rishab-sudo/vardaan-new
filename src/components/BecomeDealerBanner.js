64813176d32c8487d3b1078efac90dea8e913490
rishab-sudo <rishabdutt4@gmail.com>
2025-08-22 12:55:55 +0530

    21Aug25

--- src/components/BecomeDealerBanner.js
+++ src/components/BecomeDealerBanner.js
@@ -7,7 +7,7 @@ const BecomeDealerBanner = () => {
  <Container fluid className='becomeD-banner-section'>
 <Container className='becomeD-content-container'>
      <div className="feature-tag-heading-div">
-      <p className='page-tag-heading about-tag-heading' style={{color:"#000"}}>Become A Dealer</p>
+      {/* <p className='page-tag-heading about-tag-heading' style={{color:"#000"}}>Become A Dealer</p> */}
         </div>
 <h2 className='page-heading mt-2'>Step Into opportunity, Step Into Success</h2>
 {/* <p>ehjbfjbejhfbjebfjbehbjh</p> */}
